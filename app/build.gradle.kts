24508c2aa125870988f2815a7f75b03db97cd0db
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2026-04-07 18:05:44 -0700

    bumped version code for 0.9 full release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         minSdk = 26
         targetSdk = 28
 
-        versionCode = 13
+        versionCode = 14
         versionName = "0.9.0"
 
         buildConfigField("boolean", "GOLD", "false")
